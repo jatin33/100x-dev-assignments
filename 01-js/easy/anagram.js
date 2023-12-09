f27d6786f65a660a5966274bd5b7b241fb7c3add
Jatin <jatin.panjwani@leapfinance.com>
2023-12-09 16:59:16 +0530

    solve anagrams

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,39 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1 === str2) return true;
 
+  if (str1.length !== str2.length) return false;
+
+  if ((str1 === "" && str2.length !== 0) || (str2 === "" && str1.length !== 0)) return false;
+
+  const letterMap = str1
+    .toLowerCase()
+    .split("")
+    .filter((item) => item !== " ")
+    .reduce((acc, curr) => {
+      if (acc[curr]) {
+        acc[curr] += 1;
+      } else {
+        acc[curr] = 1;
+      }
+      return acc;
+    }, {});
+
+  let copyOfStr2 = str2
+    .toLowerCase()
+    .split("")
+    .filter((item) => item !== " ")
+    .join("");
+
+  for (let char of copyOfStr2) {
+    if (letterMap[char]) {
+      letterMap[char] -= 1;
+    }
+  }
+
+  return Object.values(letterMap).reduce((acc, curr) => acc + curr, 0) === 0;
 }
 
+console.log("isAnagram", isAnagram("hello", "world"));
 module.exports = isAnagram;

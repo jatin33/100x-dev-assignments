d7808e63887bb78bba56aeb77463902a1be52b82
Jatin <jatin.panjwani@leapfinance.com>
2023-12-09 20:00:32 +0530

    solve count vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,20 @@
 */
 
 function countVowels(str) {
+    if (str.length === 0) return 0;
     // Your code here
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const vowelsMap = new Map();
+   
+    for (const vowel of vowels) {
+      vowelsMap.set(vowel, 1);
+    }
+    return str.split("").filter((item) => item !== " ").reduce((acc, curr) => {
+      if (vowelsMap.get(curr)) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
 }
 
 module.exports = countVowels;
